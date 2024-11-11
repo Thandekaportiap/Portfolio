270a89ca75ace39983fccf21d62911492c4f8eb0
Thandekaportiap <thandekaportiap05@gmail.com>
2024-11-11 14:43:10 +0200

    url links on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <footer className="flex flex-col space-y-10 justify-center   w-full">
             <nav className="flex justify-center flex-wrap gap-6  font-medium text-2xl">
-                <a className="hover:text-white" href="#">Home</a>
-                <a className="hover:text-white" href="#">About</a>
-                <a className="hover:text-white" href="#">Contact</a>
+                <Link to="/" className="hover:text-white" >Home</Link>
+                <Link to="/about" className="hover:text-white" >About</Link>
+                <Link to="/contact" className="hover:text-white" >Contact</Link>
             </nav>
 
             <div className="flex justify-center space-x-5">
@@ -14,13 +15,10 @@ const Footer = () => {
                     <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" alt="Facebook" />
                 </a>
                 <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-                    <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" alt="LinkedIn" />
+                    <img src="https://www.linkedin.com/in/thandeka-portia-penelope-mazibuko/" alt="LinkedIn" />
                 </a>
                 <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
                     <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" alt="Instagram" />
-                </a>
-                <a href="https://messenger.com" target="_blank" rel="noopener noreferrer">
-                    <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" alt="Messenger" />
                 </a>
                 <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
                     <img src="https://img.icons8.com/fluent/30/000000/twitter.png" alt="Twitter" />
