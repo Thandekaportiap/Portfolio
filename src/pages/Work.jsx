402273a5a21f8e0992370b68f258f27647fb182b
Thandeka Portia P Mazibuko <thandekaportiap05@gmail.com>
2024-12-16 14:43:55 +0200

    added some new projects

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,129 +1,95 @@
-import React from 'react'
+import React from 'react';
 
-const sneakersImg = require('../assets/desktop-design.jpg');
-const snapLandingImg = require('../assets/desktop-design (2).jpg');
-const weatherAppImg = require('../assets/react-weather-app-screenshot.png');
-const todoAppImg = require('../assets/todoApp.png');
-const recipe = require('../assets/download.png');
+const projects = [
+  {
+    title: 'Hotel Booking App',
+    description:
+      'A feature-rich hotel booking application built with React.js, Redux, and Firebase. This app enables administrators to manage accommodations and bookings while allowing customers to browse, book rooms, and make secure payments.',
+    stack: 'React.js, Tailwind CSS, Redux, Firebase',
+    image: require('../assets/hotel app.png'),
+    alt: 'Hotel Booking App',
+    link: 'https://hotel-app-alpha.vercel.app/',
+  },
+  {
+    title: 'Snap Landing Page',
+    description:
+      'Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.',
+    stack: 'HTML, CSS, JavaScript',
+    image: require('../assets/desktop-design (2).jpg'),
+    alt: 'Snap Landing Page',
+    link: 'https://dropdownsection.netlify.app/',
+  },
+  {
+    title: 'Weather App',
+    description:
+      'A dynamic weather application developed using HTML, CSS, JavaScript and integrated with the SheCodes Weather API. This app allows users to search for real-time weather updates and 5-day forecasts for any location worldwide.',
+    stack: 'HTML, CSS, JavaScript',
+    image: require('../assets/react-weather-app-screenshot.png'),
+    alt: 'Weather App',
+    link: 'https://weatherappbytpp.netlify.app/',
+  },
+  {
+    title: 'Todo List App',
+    description:
+      'User-friendly application designed to help you manage your tasks efficiently. Easily create, edit, and delete tasks.',
+    stack: 'React.js, Tailwind CSS, SQLite, Express',
+    image: require('../assets/todoApp.png'),
+    alt: 'Todo App',
+    link: 'https://to-do-list-app-wpzq.vercel.app/',
+  },
+  {
+    title: 'Recipe App',
+    description:
+      'A user-friendly recipe app that allows users to browse, save, and share recipes. It features a searchable database, personalized collections, and detailed cooking instructions with nutritional info.',
+    stack: 'React.js, Tailwind CSS, JSON Server',
+    image: require('../assets/download.png'),
+    alt: 'Recipe App',
+    link: 'https://task-9-online-recipe.vercel.app/',
+  },
+  {
+    title: 'Shop-easy',
+    description:
+      'A powerful e-commerce platform with features like product management, user authentication, secure payments, and cart functionality.',
+    stack: 'React.js, Tailwind CSS, Redux, Firebase SDK, Node.js, Express, Stripe',
+    image: require('../assets/shopeasy.io.png'),
+    alt: 'Shop-easy',
+    link: 'https://shopeasy-io.vercel.app/',
+  },
+];
 
 const Work = () => {
   return (
-    <div className="container mx-auto px-4 py-12">
-    <p className="text-center text-3xl font-semibold mb-12">My Projects</p>
-
-    {/* Sneakers Shop Project */}
-    <div className="mb-12 flex flex-col md:flex-row items-center m-2">
-      <div className="md:w-1/2 hidden md:block">
-        <img src={sneakersImg} alt="Sneakers Shop" className="w-full h-auto rounded-lg shadow-lg" />
-      </div>
-      <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0 ml-4 p-2">
-        <div className="project-description">
-          <h2 className="text-3xl font-bold mb-5">Sneakers Shop</h2>
-          <p className="text-lg mb-5">
-            These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.
-          </p>
-          <a
-            href="https://ecommbyaa.netlify.app/"
-            className="btn btn-branding-outline bg-transparent border border-[#B1C98D] hover:bg-[#B1C98D] text-[#B1C98D] hover:text-white font-semibold py-2 px-4 rounded transition-colors"
-          >
-            Learn More
-          </a>
+    <div className="container px-4 py-12 mx-auto">
+      <p className="mb-12 text-3xl font-semibold text-center">My Projects</p>
+      {projects.map((project, index) => (
+        <div
+          key={index}
+          className={`flex flex-col items-center mb-12 ${
+            index % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse'
+          }`}
+        >
+          <div className="hidden md:w-1/2 md:block">
+            <img
+              src={project.image}
+              alt={project.alt}
+              className="w-full h-auto rounded-lg shadow-lg"
+            />
+          </div>
+          <div className="p-2 mt-8 text-center md:w-1/2 md:text-left md:mt-0">
+            <h2 className="mb-5 text-3xl font-bold">{project.title}</h2>
+            <p className="mb-5 text-lg">{project.description}</p>
+            <a
+              href={project.link}
+              className="btn btn-branding-outline bg-transparent border border-[#B1C98D] hover:bg-[#B1C98D] text-[#B1C98D] hover:text-white font-semibold py-2 px-4 rounded transition-colors"
+            >
+              Learn More
+            </a>
+            <p className="my-8">Built with {project.stack}</p>
+          </div>
         </div>
-        <p className="my-8">Built with HTML, CSS, and JavaScript</p>
-      </div>
+      ))}
     </div>
-
-    {/* Snap Landing Page Project */}
-    <div className="mb-12 flex flex-col md:flex-row items-center ">
-      <div className="md:w-1/2 text-center md:text-left order-2 md:order-1">
-        <div className="project-description">
-          <h2 className="text-3xl font-bold mb-5">Snap Landing Page</h2>
-          <p className="text-lg mb-5">
-            Get your team in sync, no matter your location. Streamline processes, create team rituals, and watch productivity soar.
-          </p>
-          <a
-            href="https://dropdownsection.netlify.app/"
-            className="btn btn-branding-outline bg-transparent border border-[#B1C98D] hover:bg-[#B1C98D] text-[#B1C98D] hover:text-white font-semibold py-2 px-4 rounded transition-colors"
-          >
-            Learn More
-          </a>
-        </div>
-        <p className="my-8">Built with HTML, CSS, and JavaScript</p>
-      </div>
-      <div className="md:w-1/2 hidden md:block order-1 md:order-2">
-        <img src={snapLandingImg} alt="Snap Landing Page" className="w-full h-auto rounded-lg shadow-lg" />
-      </div>
-    </div>
-
-    {/* Weather App Project */}
-    <div className="flex flex-col md:flex-row items-center m-2">
-      <div className="md:w-1/2 hidden md:block">
-        <img src={weatherAppImg} alt="Weather App" className="w-full h-auto rounded-lg shadow-lg" />
-      </div>
-      <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0 ml-4 p-2">
-        <div className="project-description">
-          <h2 className="text-3xl font-bold mb-5">Weather App</h2>
-          <p className="text-lg mb-5">
-            Far, far away, behind the word mountains, far from the countries Vokalia and Consonantia, live the blind texts. Separated, they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.
-          </p>
-          <a
-            href="https://weatherappbytpp.netlify.app/"
-            className="btn btn-branding-outline bg-transparent border border-[#B1C98D] hover:bg-[#B1C98D] text-[#B1C98D] hover:text-white font-semibold py-2 px-4 rounded transition-colors mb-4"
-          >
-            Learn More
-          </a>
-        </div>
-        <p className="my-8">Built with HTML, CSS, and JavaScript</p>
-      </div>
-    </div>
-    {/* Todo App Project */}
-    <div className="flex flex-col md:flex-row items-center m-2">
-      <div className="md:w-1/2 text-center md:text-left order-2 md:order-1">
-        <img src={todoAppImg} alt="todo App" className="w-full h-auto rounded-lg shadow-lg" />
-      </div>
-      <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0 ml-4 p-2">
-        <div className="project-description">
-          <h2 className="text-3xl font-bold mb-5">Todo List App</h2>
-          <p className="text-lg mb-5">
-          User-friendly application designed to help you manage your tasks efficiently. With a clean and intuitive interface, you can easily create, edit, and delete tasks, ensuring that you stay organized and productive.
-          </p>
-          <a
-            href="https://to-do-list-app-wpzq.vercel.app/"
-            className="btn btn-branding-outline bg-transparent border border-[#B1C98D] hover:bg-[#B1C98D] text-[#B1C98D] hover:text-white font-semibold py-2 px-4 rounded transition-colors mb-4"
-          >
-            Learn More
-          </a>
-        </div>
-        <p className="my-8">Built with React JS, Tailwind CSS, Sqlite and Express</p>
-      </div>
-     </div>
-
-      {/* Recipe App Project */}
-    <div className="flex flex-col md:flex-row items-center m-2">
-    <div className="md:w-1/2 hidden md:block">
-        <img src={recipe} alt="recipe app" className="w-full h-auto rounded-lg shadow-lg" />
-      </div>
-      <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0 ml-4 p-2">
-        <div className="project-description">
-          <h2 className="text-3xl font-bold mb-5">Recipe App</h2>
-          <p className="text-lg mb-5">
-          A user-friendly recipe app that allows users to browse, save, and share recipes. It features a searchable database, personalized collections, and detailed cooking instructions with nutritional info.
-          </p>
-          <a
-            href="https://task-9-online-recipe.vercel.app/"
-            className="btn btn-branding-outline bg-transparent border border-[#B1C98D] hover:bg-[#B1C98D] text-[#B1C98D] hover:text-white font-semibold py-2 px-4 rounded transition-colors mb-4"
-          >
-            Learn More
-          </a>
-        </div>
-        <p className="my-8">Built with React JS, Tailwind CSS, JSON server </p>
-      </div>
-
-
-    </div>
-  </div>
-);
+  );
 };
 
-
-export default Work
+export default Work;
